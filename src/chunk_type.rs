a93d7ee353340f1fe891bd3b8d9b1724c34a2c5e
Mahyar Mirrashed <mah.mirr@gmail.com>
2022-08-19 15:26:02 -0500

    feat: add required interface skeletons needed for ChunkType structure
    Guided by the PNGme book, we need to implement the following interfaces for the ChunkType structure
    that will be used throughout the project.

--- src/chunk_type.rs
+++ src/chunk_type.rs
@@ -1,3 +1,21 @@
+use std::{fmt::Display, str::FromStr};
+
 pub struct ChunkType {
     bytes: [u8; 4],
+}
+
+impl ChunkType {
+    //
+}
+
+impl TryFrom<[u8; 4]> for ChunkType {
+    //
+}
+
+impl FromStr for ChunkType {
+    //
+}
+
+impl Display for ChunkType {
+    //
 }
